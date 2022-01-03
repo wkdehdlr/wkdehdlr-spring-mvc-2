a3f3e81b5bafdbe9e8296e3479b6215cf55f7355
wkdehdlr <wkdehdlr@gmail.com>
2022-01-03 23:36:31 +0900

    인터셉터

--- src/main/java/com/example/springmvc2/WebConfig.java
+++ src/main/java/com/example/springmvc2/WebConfig.java
@@ -2,13 +2,24 @@ package com.example.springmvc2;
 
 import com.example.springmvc2.web.filter.LogFilter;
 import com.example.springmvc2.web.filter.LoginCheckerFilter;
+import com.example.springmvc2.web.interceptor.LogInterceptor;
 import javax.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
 
     @Bean
     public FilterRegistrationBean<Filter> logFilter() {
