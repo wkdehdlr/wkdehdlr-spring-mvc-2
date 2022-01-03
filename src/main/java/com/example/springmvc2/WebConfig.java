b73a5bced9a86552e4d38c9ee42eef25da50831a
wkdehdlr <wkdehdlr@gmail.com>
2022-01-03 22:27:25 +0900

    필터

---
+++ src/main/java/com/example/springmvc2/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.springmvc2;
+
+import com.example.springmvc2.web.filter.LogFilter;
+import javax.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean<Filter> logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+        return filterFilterRegistrationBean;
+    }
+}
