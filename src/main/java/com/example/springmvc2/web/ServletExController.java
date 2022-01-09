3ba831ab3814d45b7ade933d41496e5ecfc7d46b
wkdehdlr <wkdehdlr@gmail.com>
2022-01-09 18:59:21 +0900

    서블릿 예외처리 시작

---
+++ src/main/java/com/example/springmvc2/web/ServletExController.java
@@ -0,0 +1,27 @@
+package com.example.springmvc2.web;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class ServletExController {
+
+    @GetMapping("/error-ex")
+    public void errorEx() {
+        throw new RuntimeException("예외발생");
+    }
+
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400);
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(400);
+    }
+}
