058c52a8ef18f1e5d5b897489a44d3007c7ba319
wkdehdlr <wkdehdlr@gmail.com>
2022-01-09 19:55:38 +0900

    서블릿 예외처리 - 오류 정보 추가

--- src/main/java/com/example/springmvc2/web/exception/ErrorPageController.java
+++ src/main/java/com/example/springmvc2/web/exception/ErrorPageController.java
@@ -1,5 +1,6 @@
 package com.example.springmvc2.web.exception;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,11 @@ public class ErrorPageController {
     @RequestMapping("/error-page/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage500");
+        log.info("{}", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION));
+        log.info("{}", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE));
+        log.info("{}", request.getAttribute(RequestDispatcher.ERROR_MESSAGE));
+        log.info("{}", request.getAttribute(RequestDispatcher.ERROR_SERVLET_NAME));
+        log.info("{}", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
         return "error-page/500";
     }
 }
