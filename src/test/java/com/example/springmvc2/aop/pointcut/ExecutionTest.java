f4aa9d92a06361415ab3f772b1fa76aa94c3bada
wkdehdlr <wkdehdlr@gmail.com>
2022-02-01 21:24:07 +0900

    스프링 AOP 포인트컷 - execution1

--- src/test/java/com/example/springmvc2/aop/pointcut/ExecutionTest.java
+++ src/test/java/com/example/springmvc2/aop/pointcut/ExecutionTest.java
@@ -1,5 +1,8 @@
 package com.example.springmvc2.aop.pointcut;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.example.springmvc2.aop.member.MemberServiceImpl;
 import java.lang.reflect.Method;
 import lombok.extern.slf4j.Slf4j;
@@ -21,5 +24,72 @@ public class ExecutionTest {
     @Test
     void printMethod() {
         log.info("helloMethod = {}", helloMethod);
+    }
+
+    @Test
+    void exactMatch() {
+        // public java.lang.String com.example.springmvc2.aop.member.MemberServiceImpl.hello(java.lang.String)
+        pointcut.setExpression("execution(public String com.example.springmvc2.aop.member.MemberServiceImpl.hello(String))");
+        assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void allMatch() {
+        pointcut.setExpression("execution(* *(..))");
+        assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void nameMatch() {
+        pointcut.setExpression("execution(* hello(..))");
+        assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void namePatternMatch() {
+        pointcut.setExpression("execution(* hel*(..))");
+        assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void namePatternMatch2() {
+        pointcut.setExpression("execution(* *el*(..))");
+        assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void namePatternMatch3() {
+        pointcut.setExpression("execution(* nono(..))");
+        assertFalse(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void namePackageMatch() {
+        pointcut.setExpression("execution(* com.example.springmvc2.aop.member.MemberServiceImpl.hello(..))");
+        assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void namePackageMatch2() {
+        pointcut.setExpression("execution(* com.example.springmvc2.aop.member.*.*(..))");
+        assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void namePackageMatch3() {
+        pointcut.setExpression("execution(* com.example.springmvc2.aop.*.*.*(..))");
+        assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void namePackageMatch4() {
+        pointcut.setExpression("execution(* com.example.springmvc2.aop..*(..))");
+        assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void typeExactMatch() {
+        pointcut.setExpression("execution(* com.example.springmvc2.aop.member.MemberServiceImpl.*(..))");
+        assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
     }
 }
